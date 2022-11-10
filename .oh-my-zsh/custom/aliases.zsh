941e0a5cba3cf9e9f16b933292c2b863f7642c76
lwvemike <plamadeala.mihai002@gmail.com>
2022-11-11 00:15:51 +0200

    Added tmux config

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,9 +1,11 @@
 # Add aliases for kitty
 if [ "$TERM" = 'xterm-kitty' ]; then
 	alias icat='kitty +kitten icat'
+	alias kcopy='kitty +kitten clipboard'
 fi
 
 # Add alias for tmux
-if [ command -v tmux &> /dev/null ]; then
+if command -v tmux &> /dev/null 
+then
 	alias tmux='tmux -u'
 fi
